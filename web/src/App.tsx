01f17b2f87b61fd09b90d45a801b5b9f1d81b27f
Italo Girao <igivas26@gmail.com>
2020-06-10 20:36:36 -0300

    alteracao tag app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>teste</p>
+        <p>Italo</p>
         <a
           className="App-link"
           href="https://reactjs.org"
